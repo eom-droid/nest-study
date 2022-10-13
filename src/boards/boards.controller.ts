c2e070daa04876130b1ee3ffa13ab2e2348b7ac0
EomTangA <wer06103@gmail.com>
2022-10-14 00:54:25 +0900

    nest study first commit - 32chapter

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private boardService: BoardsService) {}
+
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.boardService.getAllBoards();
+  }
+  //if more then one param
+  // findOne(@Param() params: string[])
+  // query param vs path param
+  // query param은 ?뒤에 오는 값으로서 @Query로 사용하고 필터 작업을 할때 보통 사용
+  // path param은 /뒤에 오는 값으로서 @Param으로 사용하고 특정값을 보고 싶을때 사용
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardService.getBoardById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createBoard(@Body() createBaordDto: CreateBoardDto): Board {
+    return this.boardService.createBoard(createBaordDto);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    this.boardService.deleteBoard(id);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus,
+  ): Board {
+    return this.boardService.updateBoardStatus(id, status);
+  }
+}
